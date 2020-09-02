346772abaa3aebaecf115d17ec91745da4352d75
hhh2012aa <38385178+hhh2012aa@users.noreply.github.com>
2020-09-02 10:59:04 +0800

    new

--- bin/hls-proxy.ts
+++ bin/hls-proxy.ts
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { HlsProxyStack } from '../lib/hls-proxy-stack';
+import { nlb_stack,ga_stack } from '../lib/hls-proxy-stack';
+import {create_conf} from '../lib/create-conf'
 
 const app = new cdk.App();
-new HlsProxyStack(app, 'HlsProxyStack');
+const ivs_url = app.node.tryGetContext('ivsurl');
+create_conf(ivs_url)
+
+const my_nlb_fargate = new nlb_stack(app, 'HlsProxyStack').nlb_fargate;
+new ga_stack(app,'my-ga-stack',my_nlb_fargate)
